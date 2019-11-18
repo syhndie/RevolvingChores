1c76637b03e129ea410a15128daebff753713aeb
Cindy <syhndie2@gmail.com>
2019-11-18 16:56:23 -0600

    changed to postgres database

--- RotatingChores/Startup.cs
+++ RotatingChores/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using RotatingChores.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization;
 
 namespace RotatingChores
 {
@@ -35,9 +36,18 @@ namespace RotatingChores
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+
+            string connectionString = Configuration.GetConnectionString("DefaultConnection")
+                .Replace("secretUserId", Secrets.postgresUser)
+                .Replace("secretPassword", Secrets.postgresPassword);
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            {
+                options.UseNpgsql(connectionString);
+            });
 
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
