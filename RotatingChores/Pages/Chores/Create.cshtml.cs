75bca0f58f5d50527e591a3f7db7c529be777367
Cindy <syhndie2@gmail.com>
2019-11-21 15:16:58 -0600

    scaffolded CRUD with Entity Framework for Chore model

---
+++ RotatingChores/Pages/Chores/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RotatingChores.Data;
+using RotatingChores.Models;
+
+namespace RotatingChores.Pages.NewFolder.Chores
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RotatingChores.Data.ApplicationDbContext _context;
+
+        public CreateModel(RotatingChores.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Chore Chore { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Chores.Add(Chore);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RotatingChores/Pages/Chores/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RotatingChores.Data;
+using RotatingChores.Models;
+
+namespace RotatingChores.Pages.NewFolder.Chores
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RotatingChores.Data.ApplicationDbContext _context;
+
+        public DeleteModel(RotatingChores.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Chore Chore { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Chore = await _context.Chores.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Chore == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Chore = await _context.Chores.FindAsync(id);
+
+            if (Chore != null)
+            {
+                _context.Chores.Remove(Chore);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
