398c1a88e85a001a02ce93e76a714dba5af80f80
Cindy <syhndie2@gmail.com>
2019-11-21 15:26:01 -0600

    added BasePageModel.cs

---
+++ RotatingChores/Models/BasePageModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RotatingChores.Models
+{
+    public class BasePageModel : PageModel
+    {
+        [TempData]
+        public string DangerMessage { get; set; }
+
+        [TempData]
+        public string WarningMessage { get; set; }
+
+        [TempData]
+        public string InfoMessage { get; set; }
+
+        [TempData]
+        public string SuccessMessage { get; set; }
+    }
+}
