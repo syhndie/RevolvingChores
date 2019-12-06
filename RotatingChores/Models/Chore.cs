c5827478314dd753de15aa780bc5829c9e003a8b
Cindy <syhndie2@gmail.com>
2019-12-05 18:44:41 -0600

    changed some styling on chore index page and in chore model

--- RotatingChores/Models/Chore.cs
+++ RotatingChores/Models/Chore.cs
@@ -15,12 +15,16 @@ namespace RotatingChores.Models
         public string UserID { get; set; }
 
         [Required]
+        [Display(Name = "Chore")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [Display(Name = "Repeat every")]
         public decimal? DaysToRepeat { get; set; }
 
+        [Display(Name = "Last completed")]
+        [DataType(DataType.Date)]
         public DateTime? DateLastCompleted { get; set; }
 
         public Priority? Priority { get; set; }
