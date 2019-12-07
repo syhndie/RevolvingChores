62b1fffa02e98e54867581c93e9be3e7090687ed
Cindy <syhndie2@gmail.com>
2019-12-07 14:32:10 -0600

    added chore class, haven't finished it yet

---
+++ RotatingChores/Models/Chore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RotatingChores.Models
+{
+    public class Chore
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string UserID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateLastCompleted { get; set; }
+
+        public int FrequencyValue { get; set; }
+
+        
+
+        
+    }
+}
+//    frequencyUnit - enum (days, weeks, months, quarters, years)
+//    duedate - datetime, with a getter that uses datelast, frequencyValue, and frequencyUnit, readonly
+//    highPriority - bool, default is false, required
+//    description? notes? maybe this would only show on the details and/or edit page?, optional
