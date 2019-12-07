c3011aa0a66c3de23c870b7e2cb0c27430403d5e
Cindy <syhndie2@gmail.com>
2019-12-07 15:48:30 -0600

    completed all chore class properties

---
+++ RotatingChores/Helpers/TimeIntervals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RotatingChores.Helpers
+{
+    public enum TimeIntervals
+    {
+        days, weeks, months, years
+    }
+}
--- RotatingChores/Models/Chore.cs
+++ RotatingChores/Models/Chore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using RotatingChores.Helpers;
 
 namespace RotatingChores.Models
 {
@@ -21,12 +22,31 @@ namespace RotatingChores.Models
 
         public int FrequencyValue { get; set; }
 
+        public TimeIntervals FrequencyUnits { get; set; }
         
+        //booleans default to false, and that is what we want here
+        public bool IsHighPriority { get; set; }
 
-        
+        public string Notes { get; set; }
+
+        public DateTime DueDate
+        {
+            get
+            {
+                switch(FrequencyUnits)
+                {
+                    case TimeIntervals.days:
+                        return DateLastCompleted.AddDays(FrequencyValue);
+                    case TimeIntervals.weeks:
+                        return DateLastCompleted.AddDays(FrequencyValue * 7);
+                    case TimeIntervals.months:
+                        return DateLastCompleted.AddMonths(FrequencyValue);
+                    case TimeIntervals.years:
+                        return DateLastCompleted.AddYears(FrequencyValue);
+                    default:
+                        throw new ArgumentException("Chore has invalid Frequency Unit");
+                }
+            }
+        }    
     }
 }
-//    frequencyUnit - enum (days, weeks, months, quarters, years)
-//    duedate - datetime, with a getter that uses datelast, frequencyValue, and frequencyUnit, readonly
-//    highPriority - bool, default is false, required
-//    description? notes? maybe this would only show on the details and/or edit page?, optional
