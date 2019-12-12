6b860caaeb68f98d054c480b323800cfb0c3ff9e
Cindy <syhndie2@gmail.com>
2019-12-11 19:37:17 -0600

    added BasePageModel

---
+++ RotatingChores/Models/BasePageModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeSchoolDayBook.Models
+{
+    public class BasePageModel : PageModel
+    {
+
+        [TempData]
+        public string DangerMessage { get; set; }
+
+        [TempData]
+        public string WarningMessage { get; set; }
+
+        [TempData]
+        public string InfoMessage { get; set; }
+
+        [TempData]
+        public string SuccessMessage { get; set; }
+    }
+
+
+}
