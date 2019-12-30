d3c73d67625f742ec502a0eb726a716cb069ba68
Cindy <syhndie2@gmail.com>
2019-12-30 16:29:03 -0600

    added a contact page

---
+++ RotatingChores/Pages/Contact.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RotatingChores.Models;
+
+namespace RotatingChores.Pages
+{
+    public class ContactModel : BasePageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
