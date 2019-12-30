4fc425884eda155ccf5bd1a20c0f21d7c2ab58a2
Cindy <syhndie2@gmail.com>
2019-12-30 16:29:38 -0600

    main index takes you right to MyChores if you are logged in

--- RotatingChores/Pages/Index.cshtml.cs
+++ RotatingChores/Pages/Index.cshtml.cs
@@ -4,14 +4,61 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RotatingChores.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace RotatingChores.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel : BasePageModel
     {
-        public void OnGet()
+        private readonly UserManager<IdentityUser> _userMangaer;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public IndexModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
+            _userMangaer = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToPage("./Chores/Index");
+            }
+            else
+            {
+                return Page();
+            }
 
         }
     }
 }
+
+//@using Microsoft.AspNetCore.Identity
+// @inject SignInManager<IdentityUser> SignInManager
+//@inject UserManager<IdentityUser> UserManager
+
+//<ul class="navbar-nav">
+//    @if(SignInManager.IsSignedIn(User))
+//{
+//        < li class="nav-item">
+//            <a class="nav-link text-dark" asp-area="Identity" asp-page="/Account/Manage/Index" title="Manage">Hello @User.Identity.Name!</a>
+//        </li>
+//        <li class="nav-item">
+//            <form class="form-inline" asp-area="Identity" asp-page="/Account/Logout" asp-route-returnUrl="@Url.Page("/", new { area = "" })" method="post">
+//                <button type = "submit" class="nav-link btn btn-link text-dark">Logout</button>
+//            </form>
+//        </li>
+//    }
+//    else
+//    {
+//        <li class="nav-item">
+//            <a class="nav-link text-dark" asp-area="Identity" asp-page="/Account/Register">Register</a>
+//        </li>
+//        <li class="nav-item">
+//            <a class="nav-link text-dark" asp-area="Identity" asp-page="/Account/Login">Login</a>
+//        </li>
+//    }
+//</ul>
+
