21a9f2a9202c44f6bd01c2273804b0de2f57e62c
Cindy <syhndie2@gmail.com>
2020-01-21 15:29:01 -0600

    capitalization and other minor formatting changes

--- RotatingChores/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ RotatingChores/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -55,7 +55,7 @@ namespace RotatingChores.Areas.Identity.Pages.Account
 
                 await _emailSender.SendEmailAsync(
                     Input.Email,
-                    "Reset Password",
+                    "Reset password",
                     $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                 return RedirectToPage("./ForgotPasswordConfirmation");
