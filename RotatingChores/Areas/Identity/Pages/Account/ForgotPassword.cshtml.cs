062342765a30ba480f50626c36fef15085124368
Cindy <syhndie2@gmail.com>
2020-01-24 16:31:26 -0600

    forgot password email

--- RotatingChores/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ RotatingChores/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -8,11 +8,12 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RotatingChores.Models;
 
 namespace RotatingChores.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
-    public class ForgotPasswordModel : PageModel
+    public class ForgotPasswordModel : BasePageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly IEmailSender _emailSender;
@@ -38,10 +39,11 @@ namespace RotatingChores.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = await _userManager.FindByEmailAsync(Input.Email);
-                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user))) 
                 {
                     // Don't reveal that the user does not exist or is not confirmed
-                    return RedirectToPage("./ForgotPasswordConfirmation");
+                    SuccessMessage = "If the email address you entered is associated with a verified account, instructions to reset your password were sent.";
+                    return RedirectToPage("./Login");
                 }
 
                 // For more information on how to enable account confirmation and password reset please 
@@ -58,10 +60,12 @@ namespace RotatingChores.Areas.Identity.Pages.Account
                     "Reset password",
                     $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                return RedirectToPage("./ForgotPasswordConfirmation");
+                SuccessMessage = "If the email address you entered is associated with a verified account, instructions to reset your password were sent.";
+                return RedirectToPage("./login");
             }
 
-            return Page();
+            SuccessMessage = "If the email address you entered is associated with a verified account, instructions to reset your password were sent.";
+            return RedirectToPage("./Login");
         }
     }
 }
--- RotatingChores/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
+++ RotatingChores/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RotatingChores.Models;
 
 namespace RotatingChores.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
-    public class ForgotPasswordConfirmation : PageModel
+    public class ForgotPasswordConfirmation : BasePageModel
     {
         public void OnGet()
         {
+
         }
     }
 }
