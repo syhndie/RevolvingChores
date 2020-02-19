2aea62f5b206dcba504452fde4aa55933b893903
Cindy <syhndie2@gmail.com>
2020-02-19 16:03:03 -0600

    added real text and links to About page

--- RotatingChores/Pages/About.cshtml.cs
+++ RotatingChores/Pages/About.cshtml.cs
@@ -5,15 +5,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using RotatingChores.Areas.Identity.Data;
+using RotatingChores.Models;
+using RotatingChores.Data;
 
 namespace RotatingChores.Pages
 {
     [AllowAnonymous]
-    public class AboutModel : PageModel
+    public class AboutModel : BasePageModel
     {
+        private readonly SignInManager<RotatingChoresUser> _signInManager;
+
+        private readonly UserManager<RotatingChoresUser> _userMangaer;
+
+        private readonly ApplicationDbContext _context;
+
+        public bool IsSignedIn { get; set; }
+
+        public bool FoundAChore { get; set; }
+
+        public AboutModel(SignInManager<RotatingChoresUser> signInManager, UserManager<RotatingChoresUser> userManager, ApplicationDbContext context)
+        {
+            _signInManager = signInManager;
+            _userMangaer = userManager;
+            _context = context;
+        }
+
         public void OnGet()
         {
+            IsSignedIn = _signInManager.IsSignedIn(User);
 
+            FoundAChore = false;
+
+            if (IsSignedIn)
+            {
+                string userid = _userMangaer.GetUserId(User);
+
+                FoundAChore = _context.Chores.Any(ch => ch.RotatingChoresUserID == userid);
+            }
         }
     }
 }
