66f203a60db92f599a8d126bc3a12c7240f02d4c
Cindy <syhndie2@gmail.com>
2020-06-20 16:56:47 -0500

    javascript for changing star checkbox icon is now in site.js

--- RotatingChores/wwwroot/js/site.js
+++ RotatingChores/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function changestarcheckbox() {
+    $("#example-checkbox").change(function () {
+        var $ec = $("#example-checkbox");
+        var $icon = $("#priority-icon");
 
-// Write your Javascript code.
+        $icon.removeClass("far fas");
+      
+        if ($ec.is(":checked")) {
+            $icon.addClass("fas");
+        } else {
+            $icon.addClass("far");
+        }
+    });    
+};
